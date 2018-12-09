2f0aa3ce563fdc997a5a8412db5b901d893dc818
root <root@localhost.localdomain>
2018-12-10 02:25:52 +0539

    ddadasd

--- src/org/foo/scm/git.groovy
+++ src/org/foo/scm/git.groovy
@@ -1,8 +1,8 @@
 #!/usr/bin/groovy
 package org.foo.scm
 def getSCMInformation() {
-        def gitRemoteUrl = steps.sh(returnStdout: true, script: 'git config --get remote.origin.url').trim()
-        def gitCommitSha = steps.sh(returnStdout: true, script: 'git rev-parse HEAD').trim()
+        def gitRemoteUrl = steps.sh(script: 'git config --get remote.origin.url').trim()
+        def gitCommitSha = steps.sh(script: 'git rev-parse HEAD').trim()
         def gitBranchName = steps.sh(returnStdout: true, script: 'git name-rev --always --name-only HEAD').trim().replace('remotes/origin/', '')
 
         return [
