830f39c7fac4f8c2b7298cf843321918b50f18e6
root <root@localhost.localdomain>
2018-12-10 10:32:19 +0539

    dqddd

--- vars/var.groovy
+++ vars/var.groovy
@@ -1,1 +1,15 @@
 #!/bin/usr/groovy
+import org.foo.scm.*
+
+def call(body)
+{
+   def config = [:]
+   body.resolveStrategy = Closure.DELEGATE_FIRST
+   body.delegate = config
+   body()
+   timestamps {
+           def git = new git()
+           git.Checkout("${config.GIT_URL}","${config.BRANCH}","${config.GIT_CREDENTIALS}")
+    }
+}
+
