5cd88245f2ffaf928863796b32456ae989332c95
root <root@localhost.localdomain>
2018-12-10 13:34:31 +0539

    ccds

--- src/org/foo/scm/git.groovy
+++ src/org/foo/scm/git.groovy
@@ -19,8 +19,6 @@ def Checkout(String GIT_URL, String BRANCH, String GIT_CREDENTIALS)
           println "\u001B[32mINFO => Checking out ${GIT_URL} from branch ${BRANCH}, please wait..."
           checkout([$class: 'GitSCM', branches: [[name: "${BRANCH}"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'CloneOption', noTags: false, reference: '', shallow: true, timeout: 30]], submoduleCfg: [], userRemoteConfigs: [[credentialsId: "${GIT_CREDENTIALS}", url: "${GIT_URL}"]]])
           env.GIT_BRANCH = "${BRANCH}"
-          env.GIT_URL = "$GIT_URL"
-          env.GIT_COMMIT = getGitCommitHash()
-          env.GIT_AUTHOR_EMAIL = getCommitAuthorEmail()
+          env.GIT_URL = "${GIT_URL}"
    }
  
