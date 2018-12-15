4c8e617a07219eddc1b1ebdb44b0dcd23ed1f7aa
root <root@localhost.localdomain>
2018-12-15 21:37:04 +0539

    asasdadas

--- src/org/foo/build/maven/Sample.groovy
+++ src/org/foo/build/maven/Sample.groovy
@@ -4,7 +4,3 @@ def CleanPackage() {
    mvn_build = sh(returnStdout: true, script: 'mvn clean package') 
    return mvn_build
 }
-def GetVersion() {
-def matcher = script.readFile('pom.xml') =~ '<version>(.+?)</version>'
-        matcher ? matcher[0][1] : ""
-}
