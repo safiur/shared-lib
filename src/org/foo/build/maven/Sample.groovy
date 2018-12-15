6da0a847bbe06d652a710366921e747e98314782
root <root@localhost.localdomain>
2018-12-15 22:56:51 +0539

    aaad

--- src/org/foo/build/maven/Sample.groovy
+++ src/org/foo/build/maven/Sample.groovy
@@ -4,7 +4,19 @@ def CleanPackage(String G1, String G2) {
    mvn_build = sh(returnStdout: true, script: "mvn  ${G1} ${G2}") 
    return mvn_build
 }
+
+def getSCMInformation() {
+        def gitRemoteUrl = steps.sh(returnStdout: true, script: 'git config --get remote.origin.url').trim()
+        def gitCommitSha = steps.sh(returnStdout: true, script: 'git rev-parse HEAD').trim()
+        def gitBranchName = steps.sh(returnStdout: true, script: 'git name-rev --always --name-only HEAD').trim().replace('remotes/origin/', '')
+
+        return [
+                url: gitRemoteUrl,
+                branch: gitBranchName,
+                commit: gitCommitSha
+        ]
+ }
 def GetVersion() {
-    def mv_version = readFile('pom.xml') =~ '<version>(.+?)</version>'
-    return mv_version
+    pom = readMavenPom file: 'pom.xml'
+    return  pom.version
 }
