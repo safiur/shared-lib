89b6f18aab28468da0e43505c725113a8e2beb44
root <root@localhost.localdomain>
2018-12-16 14:09:38 +0539

    sddsfd

--- src/org/foo/build/maven/Sample.groovy
+++ src/org/foo/build/maven/Sample.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/groovy
 package org.foo.build.maven
 def CleanPackage(args) {
-    argu = args.join(" ")
+    argu = args.each{it}.join(" ")
    mvn_build = sh(returnStdout: true, script: "mvn $argu") 
    return mvn_build
 }
